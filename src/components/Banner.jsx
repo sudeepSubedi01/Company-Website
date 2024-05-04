848fb1c1f5f798c26da247edf6548c8c8b91434e
Sudip Subedi <sudip.subedi99@gmail.com>
2024-05-04 16:34:11 +0545

    banner fixed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import { ReactTyped } from 'react-typed'
 
 const Banner = () => {
   return (
-    <div className='bg-[#2699fb] w-full h-[87vh] py-[90px]' >
+    <div className='bg-[#2699fb] w-full h-[675px] py-[90px]' >
       <div className="mt-[100px] max-w-[1240px] mx-auto flex flex-col items-center gap-5">
         <div className='text-xl duration-700 md:text-3xl font-bold'>Learn with Us</div>
         <div className='text-3xl duration-700 md:text-7xl font-bold text-white'>Grow with Us</div>
